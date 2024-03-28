024f7fab08a154d1e594d484a6fd360d077ffe7c
JuanOspinaDev <juanospinav0509@gmail.com>
2024-03-28 00:29:02 -0500

    routes actualizada

--- ServicioCuentasPorPagar/api/routes.go
+++ ServicioCuentasPorPagar/api/routes.go
@@ -1,6 +1,10 @@
 package api
 
 import (
+	"ServicioCuentasPorPagar/internal/handlers"
+	"ServicioCuentasPorPagar/internal/repository"
+	"ServicioCuentasPorPagar/internal/services"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,5 +12,18 @@ import (
 func SetUpRouter() *gin.Engine {
 	router := gin.Default()
 
+	// Se realiza la inyeccion de dependencias
+	cuentaPendienteRepo := repository.NewMockCuentaPendienteRepository()
+	cuentaPendienteService := services.NewClienteService(cuentaPendienteRepo)
+	cuentaPendienteHandler := handlers.NewClienteHandler(cuentaPendienteService)
+
+	// Se establece el grupo de ruta clientes
+	clientes := router.Group("/cuentas")
+	{
+		// ruta para obtener todas las cuentas
+		clientes.GET("/", cuentaPendienteHandler.GetCuentas)
+		// ruta para obtener las cuentas por el ID del cliente
+		clientes.GET("/:clienteId", cuentaPendienteHandler.GetCuentasPorCliente)
+	}
 	return router
 }
