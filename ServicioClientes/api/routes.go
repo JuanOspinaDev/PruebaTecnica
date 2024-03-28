309369b63ae504b09cc14aca7f773f2cbc36b908
JuanOspinaDev <juanospinav0509@gmail.com>
2024-03-27 21:30:04 -0500

    Estructura inicial ServicioClientes

---
+++ ServicioClientes/api/routes.go
@@ -0,0 +1,12 @@
+package api
+
+import "github.com/gin-gonic/gin"
+
+// SetupRouter configura y retorna las rutas Gin para el manejo de Clientes.
+func SetUpRouter() *gin.Engine {
+	router := gin.Default()
+
+	// Pendiente
+
+	return router
+}
---
+++ ServicioClientes/cmd/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "ServicioClientes/api"
+
+func main() {
+	router := api.SetUpRouter()
+	router.Run(":8080")
+}
