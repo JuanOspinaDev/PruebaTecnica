b8ac0cc0f234bf84cd6b6cf64ecc972814483cc0
JuanOspinaDev <juanospinav0509@gmail.com>
2024-03-28 00:26:52 -0500

    Services para operaciones GET de ServicioCuentasPorPagar

--- ServicioCuentasPorPagar/internal/services/services.go
+++ ServicioCuentasPorPagar/internal/services/services.go
@@ -1,3 +1,32 @@
 package services
 
+import (
+	"ServicioCuentasPorPagar/internal/models"
+	"ServicioCuentasPorPagar/internal/repository"
+)
 
+// CuentaPendienteService define las funcionalidades para los clientes
+type CuentaPendienteService interface {
+	GetCuentas() ([]models.CuentaPorPagar, error)
+	GetCuentasPorCliente(clienteID int64) ([]models.CuentaPorPagar, error)
+}
+
+type cuentaPendienteService struct {
+	clienteRepo repository.CuentaPendienteRepository
+}
+
+func NewClienteService(clienteRepo repository.CuentaPendienteRepository) *cuentaPendienteService {
+	return &cuentaPendienteService{
+		clienteRepo: clienteRepo,
+	}
+}
+
+// GetClientes llama al metodo de repositorio correspondiente
+func (cp *cuentaPendienteService) GetCuentas() ([]models.CuentaPorPagar, error) {
+	return cp.clienteRepo.GetCuentas()
+}
+
+// GetClientes llama al metodo de repositorio correspondiente
+func (cp *cuentaPendienteService) GetCuentasPorCliente(clienteID int64) ([]models.CuentaPorPagar, error) {
+	return cp.clienteRepo.GetCuentasPorClienteID(clienteID)
+}
